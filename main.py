a54e0233fa3104cdf2e46b37546faefaecf62143
swaminathan33 <58352015+swaminathan33@users.noreply.github.com>
2023-11-17 10:31:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from flask import Flask, Response
+# face attendance using streamlit
+
+import streamlit as st
 import cv2
 from deepface import DeepFace
 import threading
 import datetime
 import pymongo
 
-person1 = cv2.imread('./bill.jpeg')
-person2 = cv2.imread('./elon.jpeg')
-attendant = set()
-
-f = open('attendance.txt', 'w')
+camera = cv2.VideoCapture(0)
+window = st.image([])
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["camera"]
 mycol = mydb["attendance"]
 
+run = st.checkbox('Run')
+
+person1 = cv2.imread('./bill.jpeg')
+person2 = cv2.imread('./elon.jpeg')
+attendant = set()
+
+face_match = False
 
 def recogniser(name):
     if name not in attendant:
@@ -26,7 +32,6 @@ def recogniser(name):
         }
         mycol.insert_one(add)
 
-face_match = False
 def check_face(frame):
     global face_match
     try:
@@ -43,41 +48,25 @@ def check_face(frame):
     except ValueError:
         face_match = False
 
-app = Flask(__name__)
-video = cv2.VideoCapture(0)
 
-@app.route('/')
-def index():
-    return "Default Message"
-def gen(video):
-    counter = 0
-    while True:
-        success, image = video.read()
-        if success:
-            if counter % 30 == 0:
-                try:
-                    threading.Thread(target=check_face, args=(image.copy(),)).start()
-                except ValueError:
-                    pass
-            counter += 1
+counter = 0
 
-            if face_match:
-                cv2.putText(image, 'Match !', (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+while run:
+    success, frame = camera.read()
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            else:
-                cv2.putText(image, 'No Match !', (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+    if success:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(img.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
 
-        ret, jpeg = cv2.imencode('.jpg', image)
-        frame = jpeg.tobytes()
+        if face_match:
+            cv2.putText(img, 'Match !', (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            cv2.putText(img, 'No Match !', (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
 
-@app.route('/video_feed')
-def video_feed():
-    global video
-    return Response(gen(video),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2204, threaded=True)
+    window.image(img)
