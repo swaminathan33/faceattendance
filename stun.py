fed7878427308f3f8f8b8a690126c9734b5afdf6
swaminathan33 <58352015+swaminathan33@users.noreply.github.com>
2023-11-22 07:46:10 +0530

    Update stun.py

--- stun.py
+++ stun.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['AC7f2519f83b89dfc14bf4f9f5de79e627']
-auth_token = os.environ['9d02b0abb343f657e1d7ce37caa6d016']
+account_sid = os.environ['']
+auth_token = os.environ['']
 client = Client(account_sid, auth_token)
 
 token = client.tokens.create()
